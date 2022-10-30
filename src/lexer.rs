3466e7e17316da8d2eef8a987b7640c9c2d12b59
yttnn <67893139+yttnn@users.noreply.github.com>
2022-10-30 11:07:44 +0000

    10/30

--- src/lexer.rs
+++ src/lexer.rs
@@ -9,7 +9,7 @@ pub enum Token {
   RightBrace,
   LeftBracket,
   RightBracket,
-  Commma,
+  Comma,
   Colon,
 }
 
@@ -57,12 +57,24 @@ fn next_return_token(&mut self, token: Token) -> Option<Token> {
   Some(token)
 }
 
-fn next_token(&mut self) -> Result<Option<Token>, LexerError> {
-  unimplemented!()
+fn parse_null_token(&mut self) -> Result<Option<Token>, LexerError> {
+  let s = (0..4).filter_map(|_| self.chars.next()).collect::<String>();
+  if s == "null" {
+    Ok(Some(Token::Null))
+  } else {
+    Err(LexerError::new(&format!("error: a null value is expected {}", s)))
+  }
 }
 
-fn parse_null_token(&mut self) -> Result<Option<Token>, LexerError> {
-  unimplemented!()
+#[cfg(test)]
+mod test {
+  use super::*;
+  #[test]
+  fn test_null() {
+    let null = "null";
+    let tokens = Lexer::new(null).tokenize().unwrap();
+    assert_eq!(tokens[0], Token::Null);
+  }
 }
 
 fn parse_bool_token(&mut self, b: bool) -> Result<Option<Token>, LexerError> {
@@ -79,4 +91,30 @@ fn parse_string_token(&mut self) -> Result<Option<Token>, LexerError> {
 
 fn push_utf16(result: &mut String, utf16: &mut Vec<u16>) -> Result<(), LexerError> {
   unimplemented!()
+}
+
+fn next_token(&mut self) -> Result<Option<Token>, LexerError> {
+  match self.chars.peek() {
+    Some(c) => match c {
+      c if c.is_whitespace() || *c == '\n' => {
+        Ok(self.next_return_token(Token::WhiteSpace))
+      }
+      '{' => Ok(self.next_return_token(Token::LeftBrace)),
+      '}' => Ok(self.next_return_token(Token::RightBrace)),
+      '[' => Ok(self.next_return_token(Token::LeftBracket)),
+      ']' => Ok(self.next_return_token(Token::RightBracket)),
+      ',' => Ok(self.next_return_token(Token::Comma)),
+      ':' => Ok(self.next_return_token(Token::Colon)),
+      '"' => {
+        self.chars.next();
+        self.parse_string_token();
+      }
+      c if c.is_numeric() || matches!(c, '+' | '-' | '.') => self.parse_number_token(),
+      't' => self.parse_bool_token(true),
+      'f' => self.parse_bool_token(false),
+      'n' => self.parse_bool_token(),
+      _ => Err(LexerError::new(&format!("error: an unexpected char{}", c))),
+    },
+    None => Ok(None),
+  }
 }
